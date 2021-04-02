59a8eb8aa5b69e9717525d6452d544cb42facee3
Saidarahma <80461233+Saidarahma@users.noreply.github.com>
2021-04-02 19:01:39 +0700

    Add files via upload

---
+++ I0320089_Soal1_tugas5.py
@@ -0,0 +1,7 @@
+nama = input('Siapa nama anda?')
+jenis_kelamin = input('Jenis kelamin anda?')
+if(jenis_kelamin == 'wanita'):
+    print('Selamat Datang'+','+' '+'Nyonya'+' '+str(nama)+'!!')
+else:
+    print('Selamat Datang'+','+' '+'Tuan'+' '+str(nama)+'!!')
+print()
